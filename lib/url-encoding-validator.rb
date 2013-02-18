c8d18ede892901a1e7f66db4dac8402f1bf80258
Jakub Nieznalski <jakub.nieznalski@gmail.com>
2013-02-18 13:28:08 +0100

    zen_1977_error_when_calling_cc_with_wrong_parameters

--- lib/url-encoding-validator.rb
+++ lib/url-encoding-validator.rb
@@ -1,2 +1,26 @@
 module UrlEncodingValidator
+  class Middleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      if decodeable_url_component?(env["QUERY_STRING"])
+        @app.call(env)
+      else
+        respond_with_bad_request
+      end
+    end
+
+    private
+
+    def decodeable_url_component?(component)
+      return true unless component
+      /\A[^%]*(?:%\h\h[^%]*)*\z/ =~ component ? true : false
+    end
+
+    def respond_with_bad_request
+      [400, {"Content-Type" => "text/html;", "Content-Length" => "0"}, []]
+    end
+  end
 end
--- test/url-encoding-validator_test.rb
+++ test/url-encoding-validator_test.rb
@@ -4,4 +4,15 @@ class UrlEncodingValidatorTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, UrlEncodingValidator
   end
+
+  test "return 400 response when query_string contains invalid percent encoding" do
+    assert_equal 400, UrlEncodingValidator::Middleware.new(stub).call({"QUERY_STRING" => "foobar%"}).first
+  end
+
+  test "app should receive call when params are valid" do
+    params = {"QUERY_STRING" => "foobar"}
+    app = stub
+    app.expects(:call).with params
+    UrlEncodingValidator::Middleware.new(app).call(params)
+  end
 end
