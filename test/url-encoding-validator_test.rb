4efcdd9656ae031ac0a96284ff482636fa58efc4
Michael Raidel <raidel@induktiv.at>
2013-06-10 10:46:14 +0200

    cleanup

--- test/url-encoding-validator_test.rb
+++ test/url-encoding-validator_test.rb
@@ -1,15 +1,12 @@
 require 'test_helper'
+require 'url-encoding-validator'
 
-class UrlEncodingValidatorTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, UrlEncodingValidator
-  end
-
-  test "return 400 response when query_string contains invalid percent encoding" do
+class UrlEncodingValidatorTest < Test::Unit::TestCase
+  should "return 400 response when query_string contains invalid percent encoding" do
     assert_equal 400, UrlEncodingValidator::Middleware.new(stub).call({"QUERY_STRING" => "foobar%"}).first
   end
 
-  test "app should receive call when params are valid" do
+  should "receive call when params are valid" do
     params = {"QUERY_STRING" => "foobar"}
     app = stub
     app.expects(:call).with params
