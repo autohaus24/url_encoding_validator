0934f9e0b06e27c93d5fb26466f5de0c3eb09bea
uliunger <u.unger@autohaus24.com>
2014-06-16 17:25:37 +0200

    lk_1299: update to rails 4.1

--- test/url-encoding-validator_test.rb
+++ test/url-encoding-validator_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 require 'url-encoding-validator'
 
-class UrlEncodingValidatorTest < Minitest::Unit::TestCase #MiniTest::Unit::TestCase #ActiveSupport::TestCase #Test::Unit::TestCase
+class UrlEncodingValidatorTest < Minitest::Unit::TestCase
   should "return 400 response when query_string contains invalid percent encoding" do
-    assert_equal true, true #400, UrlEncodingValidator::Middleware.new(stub).call({"QUERY_STRING" => "foobar%"}).first
+    assert_equal 400, UrlEncodingValidator::Middleware.new(stub).call({"QUERY_STRING" => "foobar%"}).first
   end
 
   should "receive call when params are valid" do
